62b4bc89b421fb7e146381002ed9468cbf6c10e4
Andrei Gorbatch <andreybach@gmail.com>
2025-05-23 16:34:16 +0100

    add basic reflection prompt

--- llm_prompts.py
+++ llm_prompts.py
@@ -19,6 +19,16 @@ Target System: {system}
 
 """
 
+REFLECTION_PROMPT = """
+You are a critic of a fantasy RPG item generator. The generatator has produced an item stat block based on a description. Critique the item stat block and provide feedback on how it could be improved.
+
+Description:
+"{description}"
+Item Stat Block:
+"{item_stat_block}"
+
+"""
+
 MAGIC_PROMPT_TEMPLATE = """
 You are the governing force of magic in a fantasy universe. Given a description of a set of circumstances that a group of mortals in your universe have constructed, determine what type of magical effect will occur.
 If there is no effect respond with: there is no magical effect.
